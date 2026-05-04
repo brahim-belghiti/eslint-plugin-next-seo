6d9ed9dcc7f943184f59b5c72812688e8e4b86cc
brahim-belghiti <brahim.belghiti@proton.me>
2026-05-04 09:37:12 +0100

    point repo URLs to brahim-belghiti/eslint-plugin-next-seo

--- src/utils/create-rule.ts
+++ src/utils/create-rule.ts
@@ -2,5 +2,5 @@ import { ESLintUtils } from "@typescript-eslint/utils";
 
 export const createRule = ESLintUtils.RuleCreator(
   (name) =>
-    `https://github.com/eslint-plugin-next-seo/eslint-plugin-next-seo/blob/main/docs/rules/${name}.md`,
+    `https://github.com/brahim-belghiti/eslint-plugin-next-seo/blob/main/docs/rules/${name}.md`,
 );
